2e18c85e507a8e8e59fc95c347cbbdb9962e8bcf
mangoALCATRAZ <mangoALCATRAZ@github.com>
2022-05-17 00:19:22 -0400

    flashy mistake trying to implement bitwise math EPILEPSY WARNING, FLASHING IMAGES AND LIGHTS

--- js/main.js
+++ js/main.js
@@ -5,26 +5,45 @@ canvas.height = h = Math.floor(innerHeight/s);
 canvas.style.width = '100%';
 canvas.style.height = '100%';
 const c = canvas.getContext("2d");
-	
+
 //let buffer1 = Array(w).fill().map(_=>Array(h).fill(0));
-let buffer1 = Array(w).fill().map(_=>Array(h).fill().map(_=>Array(3).fill(0)));
+//let buffer1 = Array(w).fill().map(_=>Array(h).fill().map(_=>Array(3).fill(0)));
+
+let buffer1 = Array(w).fill().map(_=>new Uint32Array(h));
+
 
 //let buffer2 = Array(w).fill().map(_=>Array(h).fill(0));
-let buffer2 = Array(w).fill().map(_=>Array(h).fill().map(_=>Array(3).fill(0)));
+//let buffer2 = Array(w).fill().map(_=>Array(h).fill().map(_=>Array(3).fill(0)));
+
+let buffer2 = Array(w).fill().map(_=>new Uint32Array(h));
 
 const damping = 0.999999;
 let temp;
 
+let blue = 0;
+let green = 0;
+let red = 255;
 function animation(){
 	
 	for(let i = 1; i < w-1; i++){
 		for(let j = 1; j < h-1; j++){
-			/*buffer2[i][j] = ((buffer1[i-1][j] +
+			/*
+				original algorithm
+
+				buffer2[i][j] = ((buffer1[i-1][j] +
 											 buffer1[i+1][j] +
 											 buffer1[i][j-1] +
 											 buffer1[i][j+1]) / 2 - buffer2[i][j]) * damping;
 			*/
 
+
+			//bit manipulation version of algorithm
+			buffer2[i][j] = ((buffer1[i-1][j] ^
+							  buffer1[i+1][j] ^
+							  buffer1[i][j-1] ^
+							  buffer1[i][j+1]));
+
+		/*
 			// morph red
 			buffer2[i][j][0] = ((buffer1[i-1][j][0] +
 								 buffer1[i+1][j][0] +
@@ -42,24 +61,37 @@ function animation(){
 								 buffer1[i+1][j][2] +
 								 buffer1[i][j-1][2] +
 								 buffer1[i][j+1][2]) / 2 - buffer2[i][j][2] * damping);
+
+		 */
 		}
 	}
 	
 	let img = new ImageData(w, h)
+	let buf = new ArrayBuffer(img.data.length);
+	let buf8 = new Uint8ClampedArray(buf);
+	let data = new Uint32Array(buf);
 	
 	for(let i = 0; i < buffer1.length; i++){
 		for(let j = 0; j < buffer1[0].length; j++){
-			let index = (j * buffer1.length + i) * 4
-			img.data[index] = buffer2[i][j][0]
-			img.data[index+1] = buffer2[i][j][1]
-			img.data[index+2] = buffer2[i][j][2]
+			let index = (j * buffer1.length + i);
 
+			data[index] = buffer1[i][j];
+
+			//original color attempt
+			//img.data[index] = buffer2[i][j][0]
+			//img.data[index+1] = buffer2[i][j][1]
+			//img.data[index+2] = buffer2[i][j][2]
+
+
+			//original
 			//img.data[index] = buffer2[i][j]
 			//img.data[index+1] = buffer2[i][j]
 			//img.data[index+2] = buffer2[i][j]
-			img.data[index+3] = 255
+			//img.data[index+3] = 255
 		}
 	}
+
+	img.data.set(buf8);
 	
 	c.putImageData(img,0,0)
 	
@@ -74,6 +106,25 @@ animation();
 function ripple(e){
 	let x = Math.floor(e.clientX/s);
 	let y = Math.floor(e.clientY/s);
+
+	if(blue == 0) blue = 255;
+	else if(blue == 255) blue = 0;
+	if (red == 255) red = 0;
+	else if(red == 0) red = 255;
+
+	let value =
+		(255 << 24) |  //alpha
+		(blue << 16) | //blue
+		(green << 8) | //green
+		red;		   //red
+
+	buffer1[x][y] = value;
+	buffer1[x+1][y] = value;
+	buffer1[x+2][y] = value;
+	buffer1[x][y+1] = value;
+	buffer1[x][y+1] = value;
+	/*
+
 	//red test
 	buffer1[x][y][0] = 490; // R
 
@@ -94,6 +145,9 @@ function ripple(e){
 	buffer1[x+1][y+1][2] = 14; // B
 
 	//buffer1[x][y] = 500;
+
+
+	 */
 }
 
 document.addEventListener("click", ripple  );
@@ -106,4 +160,4 @@ window.addEventListener("resize",function(){
 
 
 //debug test click immediately after script execution
-document.elementFromPoint(0, 0).click();
+//document.elementFromPoint(0, 0).click();
